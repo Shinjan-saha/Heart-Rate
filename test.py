905a1578852b3816b32d808085d56070e9159cf8
Shinjan-saha <shinjansaha02@gmail.com>
2023-11-29 17:02:25 +0530

    initials Commit

---
+++ test.py
@@ -0,0 +1,28 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+audio_file = 'm96.wav'
+
+
+y, sr = librosa.load(audio_file)
+
+
+plt.figure(figsize=(14, 5))
+librosa.display.waveshow(y, sr=sr)
+plt.title('Waveform')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.show()
+
+
+plt.figure(figsize=(14, 5))
+D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+plt.title('Spectrogram')
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+plt.colorbar(format='%+2.0f dB')
+plt.show()
